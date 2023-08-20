b5bce87604c722998838cc72321a0ee33e310dd2
Borngod <aboa.boni360@gmail.com>
2023-08-20 09:10:56 +0000

    worked on project

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+   
+});
+
+  
